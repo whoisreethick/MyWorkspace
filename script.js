8665f4275a0f4332ff07916e19cfa84197aebabb
Reethick Rosan <143926519+whoisreethick@users.noreply.github.com>
2024-12-03 11:49:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+/*var typed=new Typed(".text", {
+    strings: ["Student, ", "Aspiring Learner, ", "Full Stack Developer."],
+    typeSpeed: 100,
+    backSpeed: 200,
+    backDelay: 1000,
+    loop: true
+})*/
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed(".text", {
+        strings: ["Student. ", "Aspiring Learner. ", "Full Stack Developer."],
+        typeSpeed: 200,
+        backSpeed: 200,
+        backDelay: 200,
+        loop: true
+    });
+});
